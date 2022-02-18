10f6e06da0c2c39c74d42d84efef74eb7d743df1
Brendan Bax <brendanbax@gmail.com>
2022-02-17 23:05:29 -0600

    renames things

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:brendanbax/2-7-study.git master:gh-pages
+git push -f git@github.com:brendanbax/27_study.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   pwa: {
-    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/27_study/' : '/',
     name: "2:7 Study",
     themeColor: "#2b313b",
     display: 'standalone',
