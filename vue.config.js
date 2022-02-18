00b2a89666529846b90a067bbb05269f9cf4c85f
Brendan Bax <brendanbax@gmail.com>
2022-02-17 23:43:50 -0600

    simplify config for deploy

--- vue.config.js
+++ vue.config.js
@@ -5,13 +5,5 @@ module.exports = {
     themeColor: "#2b313b",
     display: 'standalone',
     backgroundColor: '#2b313b',
-    icons: {
-      faviconSVG: 'img/icons/favicon.svg',
-      favicon32: 'img/icons/favicon-32x32.png',
-      favicon16: 'img/icons/favicon-16x16.png',
-      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
-      maskIcon: 'img/icons/safari-pinned-tab.svg',
-      msTileImage: 'img/icons/msapplication-icon-144x144.png'
-    },
   }
 };
